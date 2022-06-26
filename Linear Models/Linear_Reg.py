19036dd60b8a77ae487af5e8bad44708a9542ffe
Leon-Parepko <l.parepko@innopolis.university>
2022-06-26 23:14:11 +0300

    Working Linear Regression.

--- Linear Models/Linear_Reg.py
+++ Linear Models/Linear_Reg.py
@@ -1,14 +1,11 @@
 import numpy as np
-import pandas as pd
 from tqdm import tqdm
 
 
 def init_weights(X):
-    random_weights = np.random.rand(np.shape(X)[1] + 1)
+    random_weights = np.random.randn(np.shape(X)[1] + 1)
     return random_weights
 
-def mse():
-    pass
 
 def add_ones_col(X):
     return np.c_[np.ones(np.shape(X)[0]), X]
@@ -16,7 +13,6 @@ def add_ones_col(X):
 class LinearRegression:
 
     def __init__(self, loss="MSE"):
-        # if loss == "MSE":
         self.loss = loss
         self.X_Data = []
         self.Y_Data = []
@@ -31,31 +27,33 @@ class LinearRegression:
 
     def predict(self, X_input):
         out = np.array([])
-        X_input = add_ones_col(X_input)
         if np.ndim(X_input) > 1:
+            X_input = add_ones_col(X_input)
             for individ in X_input:
-                result = self.weights @ individ
+                result = individ @ self.weights
                 out = np.append(out, result)
         else:
-            out = self.weights @ X_input
+            X_input = np.append(1, X_input)
+            out = X_input @ self.weights
 
         return out
 
 
-    def train(self, learning_rate=0.1):
-
+    def train(self, lr=0.1, iter=100):
         X = add_ones_col(self.X_Data)
         features_num = np.shape(X)[1]
-        for i in tqdm(range(0, np.shape(X)[0])):
-            d_loss = (1 / features_num) * np.transpose(X[i, :]) * (self.predict(self.X_Data[i, :]) - self.Y_Data[i])
-            self.weights = self.weights - learning_rate * d_loss
+        for _ in tqdm(range(0, iter)):
+            for i in range(0, np.shape(X)[0]):
+                d_loss = (2 / features_num) * (self.predict(self.X_Data[i, :]) - self.Y_Data[i]) * X[i, :]
+                self.weights = self.weights - lr * d_loss
 
 
 if __name__ == '__main__':
 
-    X = np.random.rand(21613, 1)
-    Y = np.random.rand(21613, 1)
+    X = np.random.rand(2000, 50)
+    Y = np.random.rand(2000, 1)
 
     L_R = LinearRegression()
     L_R.fit(X, Y)
-    L_R.train()
+    L_R.train(lr=0.01, iter=100)
+    print(L_R.weights)
