9e194566fd3fc3b396118dd96f03265c83444132
Leon-Parepko <l.parepko@innopolis.university>
2022-07-04 19:03:22 +0300

    Add some comments and clean-up.

--- Classic_ML/Decision_Tree.py
+++ Classic_ML/Decision_Tree.py
@@ -1,26 +1,20 @@
 
 import numpy as np
 from tqdm import tqdm
-from sklearn import preprocessing
 
-
-def info_gain(parent, left_ch, right_ch, method="gini"):
+# Info gain is used to find the best split (use entropy or gini index)
+def info_gain(parent, left_ch, right_ch, method):
     p_size = parent.size
     l_size = left_ch.size
     r_size = right_ch.size
-    ig = 0
 
-    if method == "entropy":
-        ig = entropy(parent) - \
-             (l_size / p_size) * entropy(left_ch) - \
-             (r_size / p_size) * entropy(right_ch)
-    elif method == "gini":
-        ig = entropy(parent) - \
-             (l_size / p_size) * gini(left_ch) - \
-             (r_size / p_size) * gini(right_ch)
+    ig = method(parent) - \
+         (l_size / p_size) * method(left_ch) - \
+         (r_size / p_size) * method(right_ch)
     return ig
 
 
+# Entropy formula
 def entropy(Y):
     total_obj = Y.size
     unique, counts = np.unique(Y, return_counts=True)
@@ -28,10 +22,9 @@ def entropy(Y):
     for elem in counts:
         class_prob = elem / total_obj
         entropy -= class_prob * np.log2(class_prob)
-
     return entropy
 
-
+# Gini index formula
 def gini(Y):
     total_obj = Y.size
     unique, counts = np.unique(Y, return_counts=True)
@@ -42,21 +35,22 @@ def gini(Y):
     return 1 - gini
 
 
-
-
-def check_combinations(X, Y):
+# Check all the possible combinations of features
+#  and it's values and get the best possible
+#  split using information gain.
+def get_best_split(X, Y):
     curr_IG = 0
     l_ch = None
     r_ch = None
     condition = None
 
-# Iterate for each i-individ and j-feature
-    for i in range(0, X.shape[0]):
+    # Iterate for each i-individ and j-feature
+    for i in tqdm(range(0, X.shape[0])):
         for j in range(0, X.shape[1]):
             elem = X[i, j]
 
-        # Introduce arrays to store the data values
-        #  for left and right child
+            # Introduce arrays to store the data values
+            #  for left and right child
             itr = 0
             l_Y_arr = np.array([])
             r_Y_arr = np.array([])
@@ -83,13 +77,14 @@ def check_combinations(X, Y):
                         r_Y_arr = np.vstack((r_Y_arr, corresp_Y))
                 itr += 1
 
-            IG = info_gain(Y, l_Y_arr, r_Y_arr)
+            # Check information gain and save max in left and right node.
+            IG = info_gain(Y, l_Y_arr, r_Y_arr, gini)
             if curr_IG < IG:
                 l_ch = Node(None, l_X_arr, l_Y_arr, leaf=True)
                 r_ch = Node(None, r_X_arr, r_Y_arr, leaf=True)
 
 
-            # Set prediction if there is only one class in node
+                # Set prediction if there is only one class in node.
                 l_unique, l_counts = np.unique(l_Y_arr, return_counts=True)
                 r_unique, r_counts = np.unique(r_Y_arr, return_counts=True)
                 if len(l_unique) == 1:
@@ -106,8 +101,9 @@ def check_combinations(X, Y):
 
 
 
-
-
+"""
+    Node class for tree.
+"""
 class Node:
     def __init__(self, condition, X, Y, left_ch=None, right_ch=None, root=False, leaf=False, prediction=None):
         self.condition = condition
@@ -120,12 +116,17 @@ class Node:
         self.prediction = prediction
 
 
+"""
+    Main tree class.
+"""
 class DecisionTree():
     def __init__(self):
         self.root = None
         self.depth = 0
+        self.nodes = 0
 
 
+    # Return all leafs that could be modified.
     def get_leafs(self, root, leaf_list=None):
         if leaf_list is None:
             leaf_list = []
@@ -146,49 +147,51 @@ class DecisionTree():
         return leaf_list
 
 
+    # Save data to variables.
     def fit(self, X, Y):
         self.X = X
         self.Y = Y
 
 
+    # Train model.
     def train(self, max_depth=4):
-
-    # Create initial root node before the main loop
-        cond, l_ch, r_ch, = check_combinations(self.X, self.Y)
-
+        # Create initial root node before the main loop
+        cond, l_ch, r_ch, = get_best_split(self.X, self.Y)
         self.root = Node(cond, self.X, self.Y, left_ch=l_ch, right_ch=r_ch, root=True)
         self.depth += 1
 
         while True:
             leaf_modif_list = self.get_leafs(self.root)
 
-        # Set predictions if the tree is not
-        #  completed but have maximal depth
+            # Set predictions if the tree is not
+            #  completed but have maximal depth
             if self.depth == max_depth:
                 for leaf in leaf_modif_list:
                     leaf.prediction = np.mean(leaf.Y)
                     leaf.leaf = True
                 break
 
-        # Break if the tree is complete
+            # Break if the tree is complete
             if not leaf_modif_list:
                 break
 
             for leaf in leaf_modif_list:
-                cond, l_ch, r_ch, = check_combinations(leaf.X, leaf.Y)
+                cond, l_ch, r_ch, = get_best_split(leaf.X, leaf.Y)
                 leaf.left_ch = l_ch
                 leaf.right_ch = r_ch
                 leaf.condition = cond
                 leaf.leaf = False
+                self.nodes += 1
             self.depth += 1
 
 
+    # Predict single individ (if X is a vector).
     def predict_single(self, root, X):
         pred = root.prediction
         cond = root.condition
         out = None
 
-        # Base case when there is prediction
+        # Base case when there is prediction.
         if pred is not None:
             return pred
 
@@ -198,9 +201,10 @@ class DecisionTree():
                 out = self.predict_single(root.left_ch, X)
             else:
                 out = self.predict_single(root.right_ch, X)
-
         return out
 
+
+    # Predict multiple individs using 'predict_single' method.
     def predict_many(self, X):
         out = []
         for elem in X:
