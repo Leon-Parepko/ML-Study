7aec131b432f1b0a8edb52c43016fe5fd556befc
Leon-Parepko <l.parepko@innopolis.university>
2022-08-01 22:48:17 +0300

    Add NN and CNN for mnist dataset (yaaay... everything works :))

--- Deep_Learning/Mnist_Classifier_CNN.py
+++ Deep_Learning/Mnist_Classifier_CNN.py
@@ -1,7 +1,36 @@
 
-import torch
-import torchvision
-import numpy as np
+import torch.nn as nn
 
-if __name__ == '__main__':
-    pass
+
+class MNIST_CNN(nn.Module):
+    def __init__(self):
+        super(MNIST_CNN, self).__init__()
+        self.conv_model = nn.Sequential(
+            nn.Conv2d(1, 10, 3, stride=1),
+            nn.ReLU(),
+
+            nn.Conv2d(10, 30, 3, stride=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+
+            nn.Conv2d(30, 50, 3, stride=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+
+            # nn.Conv2d(60, 100, 1, stride=1),
+            # nn.ReLU(),
+            # nn.MaxPool2d(2),
+            )
+
+        self.lin_model = nn.Sequential(
+            nn.Linear(50*5*5, 250),
+            nn.ReLU(),
+
+            nn.Linear(250, 10),
+            )
+
+    def forward(self, X):
+        out = self.conv_model(X)
+        out = out.view(-1, 50*5*5)
+        out = self.lin_model(out)
+        return out
--- Deep_Learning/Mnist_Classifier_NN.py
+++ Deep_Learning/Mnist_Classifier_NN.py
@@ -1,7 +1,19 @@
 
-import torch
-import torchvision
-import numpy as np
+import torch.nn as nn
 
-if __name__ == '__main__':
-    pass
+
+class MNIST_NN(nn.Module):
+    def __init__(self, input_size, output_size):
+        super(MNIST_NN, self).__init__()
+        self.model = nn.Sequential(
+            nn.Linear(input_size, 512),
+            nn.ReLU(),
+
+            nn.Linear(512, 256),
+            nn.ReLU(),
+
+            nn.Linear(256, output_size),
+        )
+
+    def forward(self, X):
+        return self.model(X)
