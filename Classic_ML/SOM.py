885e6cd3c59389e525dbee1196a736878b0da011
Leon-Parepko <l.parepko@innopolis.university>
2023-02-16 13:55:45 +0300

    Finaly add working SOM model.

--- Classic_ML/SOM.py
+++ Classic_ML/SOM.py
@@ -1,29 +1,40 @@
 import numpy as np
 
-''' 
-    Initialize random weights vector
-'''
+
+
 def init_weights(features_num, map_size):
+    """
+        Initialize random weights vector
+    """
     random_weights = np.random.randn(features_num, map_size[0], map_size[1])
     return random_weights
 
 
-'''
-    Function to add colum of ones to the matrix
-        (bias term)
-'''
 def add_ones_col(X):
+    """
+        Function to add colum of ones to the matrix
+        (bias term)
+    """
     return np.c_[np.ones(np.shape(X)[0]), X]
 
 
-def softmax(x):
-    e_x = np.exp(x)
-    return e_x / e_x.sum(axis=0)
-
-
 def get_indexes_in_radius(array, center, radius):
+    """ """
     a = np.indices(array.shape).reshape(2, -1).T
     return a[np.abs(a-np.array(center)).sum(1) <= int(radius)]
+
+
+def get_win_neuron(weights, X, map_size):
+    """ """
+    X_for_W_update = np.tile(X, (map_size[0], map_size[1], 1)).T        # Stack X into tensor to update weights
+
+    win_tensor = weights - X_for_W_update
+    win_matrix = np.linalg.norm(win_tensor, axis=0)                     # Calculate length of vectors (W - X)
+
+    winner = np.argmin(win_matrix)                                      # Get minimum value index
+    winner_coord = (winner // map_size[0], winner % map_size[1])
+    return winner_coord
+
 
 
 class SOM:
@@ -31,32 +42,64 @@ class SOM:
         self.map_size = map_size
         self.features_num = features_num
         self.weights = []
-        self.Y = np.zeros(self.map_size)
-        self.weights = init_weights(features_num, self.map_size)    # Random weights
-        self.win_matrix = np.zeros(self.map_size)                   # Matrix of distances between weights and input vector
-        self.label_map = np.zeros(self.map_size)                    # Matrix of labels
+        self.map = np.zeros(self.map_size)
+        self.weights = init_weights(features_num, self.map_size)        # Random weights
+        self.win_matrix = np.zeros(self.map_size)                       # Matrix of distances between weights and input vector
+        self.weights_history = []
+        self.map_history = []
 
 
+    def predict(self, X):
+        """ """
+        winner_coord = get_win_neuron(self.weights, X, self.map_size)
+        return self.map[winner_coord[0]][winner_coord[1]]
 
-    def train(self, X, lr=0.1, iter=100, radius=1):
-        X_for_W_update = np.tile(X, (self.map_size[0], self.map_size[1], 1)).T        # Stack X into tensor to update weights
 
-        for _ in range(0, iter):                                     # Start of algorithm
-            win_tensor = self.weights - X_for_W_update
-            self.win_matrix = np.linalg.norm(win_tensor, axis=0)        # Calculate length of vectors (W - X)
+    def create_map(self, X_all, Y_all):
+        """ """
+        label_map = np.full(self.map_size, 0, dtype=object)             # Create empty map
 
-            winner = np.argmin(self.win_matrix)      # Get minimum value index
-            winner_coord = (winner // self.map_size[0], winner % self.map_size[1])
+        # Fill label_map with empty lists
+        for i in range(self.map_size[0]):
+            for j in range(self.map_size[1]):
+                label_map[i][j] = []
 
-            for i, neuron_coord in enumerate(get_indexes_in_radius(self.Y, winner_coord, radius)):       # Update weights of all the neurons in radius
-                distance_to_winner = np.abs(winner_coord[0] - neuron_coord[0]) + np.abs(winner_coord[1] - neuron_coord[1])   # Topological distance (not Euclidean)
+        for i in range(X_all.shape[0]):
+            X = X_all[i]
+            Y = Y_all[i]
+            winner_coord = get_win_neuron(self.weights, X, self.map_size)
+            label_map[winner_coord[0]][winner_coord[1]].append(Y)
+
+        for i in range(self.map_size[0]):
+            for j in range(self.map_size[1]):
+                arr = label_map[i][j]
+
+                if len(arr) == 0:
+                    self.map[i][j] = -1
+                else:
+                    self.map[i][j] = max(arr, key=arr.count)
+
+
+    def train(self, data, iter, lr=0.1, radius=1, weight_history=False, map_history=False, Y = None):
+        """ """
+
+        self.weights_history = []
+        self.map_history = []
+        for i in range(iter):
+            X = data[np.random.randint(0, data.shape[0])]                                                                       # Get random X vector from data
+
+            winner_coord = get_win_neuron(self.weights, X, self.map_size)
+
+            for neuron_coord in get_indexes_in_radius(self.map, winner_coord, radius):                                          # Update weights of all the neurons in radius
+                distance_to_winner = np.abs(winner_coord[0] - neuron_coord[0]) + np.abs(winner_coord[1] - neuron_coord[1])      # Topological distance (Manhattan distance)
                 distance_to_winner = np.power(0.1, distance_to_winner)
 
-                W_vec = self.weights[:, neuron_coord[0], neuron_coord[1]]       # Get weights vector of neuron
+                W_vec = self.weights[:, neuron_coord[0], neuron_coord[1]]                                                       # Get weights vector of neuron
                 self.weights[:, neuron_coord[0], neuron_coord[1]] += distance_to_winner * lr * (X - W_vec)           # (1 - lr * distance_to_winner) * W_vec + lr * distance_to_winner * X    # Update weights of a neuron
 
+            if weight_history and i % 300 == 0:
+                self.weights_history.append(np.copy(self.weights))
 
-
-
-if __name__ == '__main__':
-    pass
+            if map_history and i % 300 == 0:
+                self.create_map(data, Y)
+                self.map_history.append(np.copy(self.map))
